fc8434c8a138b32a3ad0530c3ea0df6f882445bf
JaneYTLing <janeytling@protonmail.com>
2023-07-12 15:56:46 +0100

    added function for changing selection from plot

--- main.py
+++ main.py
@@ -17,44 +17,66 @@ import numpy as np
 filepath = 'testdata/originals/'
 savepath = 'testdata/outputs/'
 
+# ------------------------------------------------------------------#
+#                     predefine ROIs (optional)                     #
+# ------------------------------------------------------------------#
 # Selecting cells to be processed. If set to None, process all ROIs
 # cells_to_process = np.linspace(0, 49, endpoint=False, dtype='int')
 # Selecting cells to be plotted
 # cells_to_plot = [7, 8]
 
+# ------------------------------------------------------------------#
+#                          creating object                          #
+# ------------------------------------------------------------------#
 # load .npy bindata from read_path
 s2p = st.s2p(filepath, savepath)  # for test dataset
 # s2p = st.s2p(filepath, savepath, cells_to_process, cells_to_plot)  # for test dataset
 s2p.create_metadata()
 
-# Selecting cells to be processed. If set to None, process all ROIs
-# cells_to_process = np.linspace(0, 5, 4, endpoint=False, dtype='int')
-cells_to_process = [0, 1, 2, 3, 4, 5]
+# ------------------------------------------------------------------#
+#       change selection of ROI by list of numbers (optional)       #
+# ------------------------------------------------------------------#
+# # Selecting cells to be processed. If set to None, process all ROIs
+# # cells_to_process = np.linspace(0, 5, 4, endpoint=False, dtype='int')
+# cells_to_process = [0, 1, 2, 3, 4, 5]
+#
+# # Selecting cells to be plotted
+# cells_to_plot = [0, 2]
+# s2p.change_cell_selection(cells_to_process, cells_to_plot)
+# # Or for just change in one set of selection, use:
+# # s2p.change_cell_selection(cells_to_process)
+# # s2p.change_cell_selection(cells_to_plot)
 
-# Selecting cells to be plotted
-cells_to_plot = [0, 2]
-s2p.change_cell_selection(cells_to_process, cells_to_plot)
-# Or for just change in one set of selection, use:
-# s2p.change_cell_selection(cells_to_process)
-# s2p.change_cell_selection(cells_to_plot)
 
-# initialize plot average fluorescence
-s2p.im_plot('avg_bin', plot=True, filename='average_binary.tif')
+# ------------------------------------------------------------------#
+#                           plotting figures                        #
+# ------------------------------------------------------------------#
+# # initialize plot average fluorescence
+# s2p.im_plot('avg_bin', plot=True, filename='average_binary.tif')
+#
+# # initialize plot for selected cells at peak intensity
+# s2p.im_plot('selected_cells', plot=True, filename='selected_cells.tif')
+#
+# # initialize plot for selected cells with contours
+# s2p.im_plot('contour', plot=True, filename='contours.tif')
+#
+# # initialize plot for selected cells with contours and major and minor axes
+# s2p.im_plot('axis', plot=True, filename='axes.tif')
+#
+# # plot and save figures
+# s2p.plot_fig()
+
+# ------------------------------------------------------------------#
+#            change selection of ROI from figure (optional)         #
+# ------------------------------------------------------------------#
+s2p.cells_to_process_from_fig()
+# s2p.cells_to_plot_from_fig()
 
 # initialize plot for selected cells at peak intensity
 s2p.im_plot('selected_cells', plot=True, filename='selected_cells.tif')
 
-# initialize plot for selected cells with contours
-s2p.im_plot('contour', plot=True, filename='contours.tif')
-
-# initialize plot for selected cells with contours and major and minor axes
-s2p.im_plot('axis', plot=True, filename='axes.tif')
-
 # plot and save figures
 s2p.plot_fig()
-
-
-
 
 
 
